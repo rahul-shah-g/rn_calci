34721d93d51884ec3fb373f04183fc0557b8ca57
rahul-shah-g <rahulshah.gkp@gmail.com>
2024-02-25 18:04:47 +0530

    added react antive skia

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,40 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
 
 const Home: React.FC = () => {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      value: 'sfsgg',
+    },
+    {
+      id: 2,
+      value: 'sfsgg',
+    },
+    {
+      id: 3,
+      value: 'sfsgg',
+    },
+    {
+      id: 5,
+      value: 'sfsgg',
+    },
+  ]);
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{flex: 1, backgroundColor: '#ff'}}>
+      <Text>{'8988'}</Text>
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => {
+          return (
+            <View>
+              <Text>{item.value}</Text>
+            </View>
+          );
+        }}
+        extraData={data}
+      />
     </View>
   );
 };
